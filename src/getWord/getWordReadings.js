7fc8cc2a5ff6ad4446d174bdb1d76e2c2df135e5
root <root@DESKTOP-51MGIR3.localdomain>
2020-09-08 17:05:56 +0200

    changed getReading logic

--- src/getWord/getWordReadings.js
+++ src/getWord/getWordReadings.js
@@ -1,17 +1,33 @@
-import uniq from 'lodash';
+import uniq from 'lodash/uniq';
+import { isEmpty } from 'lodash';
 import fetchJisho from './fetchJisho';
+
+const hasPerfectMatch = (word, { word: resWord, reading }) => (
+  (word === resWord || word === reading) && reading
+);
+
+const hasMatch = (word, { word: resWord, reading = '' }) => (
+  (
+    word.includes(resWord)
+    || resWord.includes(word)
+    || word.includes(reading)
+    || reading.includes(word)
+  ) && reading
+);
 
 const getWordReadings = async (_, word) => {
   try {
     const response = await fetchJisho(word);
-    const furiganaList = uniq(
-      response.reduce((acc, { japanese }) => ([
-        ...acc,
-        ...japanese,
-      ]), [])
-        .filter(({ word: resWord, reading }) => word === resWord && reading)
-        .map(({ reading }) => reading),
-    );
+    const reducedResponse = response.reduce((acc, { japanese }) => ([
+      ...acc,
+      ...japanese,
+    ]), []);
+
+    const perfectMatch = reducedResponse.filter((r) => hasPerfectMatch(word, r));
+    const match = isEmpty(perfectMatch)
+      ? reducedResponse.filter((r) => hasMatch(word, r))
+      : [];
+    const furiganaList = uniq([...perfectMatch, ...match].map(({ reading }) => reading));
     return Promise.resolve(furiganaList);
   } catch (err) {
     return Promise.reject(err);
